9b2b4293980bf310bc225ae1979d60602cd1691f
Fernando Guillen <fguillen.mail@gmail.com>
2023-03-28 10:37:18 +0200

    Cards are selected

--- actors/card_combat.gd
+++ actors/card_combat.gd
@@ -3,17 +3,48 @@ extends Control
 
 signal selected(card_combat: CardCombat)
 
+@onready var click_area: Area2D = $ClickArea2D
+
 var value: CardValue
 
 
 func setup(card_value: CardValue):
 	self.value = card_value
-
+	
+	
+func reparent_and_jump(new_parent: Node2D):
+	print("CardCombat.reparent_and_jump")
+	
+	# Deactivate Click
+	click_area.monitoring = false
+	
+	# Reparent
+	var actual_position = global_position
+	get_parent().remove_child(self)
+	new_parent.add_child(self)
+	global_position = actual_position
+	
+	# Jump
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position", new_parent.global_position, 1.0)
+	
 
 func _select():
 	emit_signal("selected", self)
 
 
 func _on_click_area_2d_input_event(viewport, event, shape_idx):
+	print("CardCombat._on_click_area_2d_input_event()")
+	
 	if Input.is_action_just_pressed("click"):
+		_select()
+
+
+func _on_gui_input(event):
+	print("CardCombat._on_gui_input()")
+	print("CardCombat._on_gui_input(): ", event.get_class())
+	if event is InputEventMouseButton:
+		print("CardCombat._on_gui_input(): ", event.button_index)
+	
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		_select()
--- scenes/combat.gd
+++ scenes/combat.gd
@@ -13,6 +13,10 @@ var cards_monster: Array[CardCombat] = []
 var cards_oponent: Array[CardCombat] = []
 
 
+func select_card(card_combat: CardCombat, card_placeholder: Node2D):
+	card_combat.reparent_and_jump(card_placeholder)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var card_value = CardValue.new()
@@ -29,9 +33,11 @@ func _set_up_decks():
 		card_combat.setup(card_value)
 		cards_monster.append(card_combat)
 		deck_container_monster.add_child(card_combat)
+		card_combat.selected.connect(select_card.bind(card_placeholder_monster))
 		
 	for card_value in card_values_oponent:
 		var card_combat := card_combat_scene.instantiate() as CardCombat
 		card_combat.setup(card_value)
 		cards_oponent.append(card_combat)
 		deck_container_oponent.add_child(card_combat)
+		card_combat.selected.connect(select_card.bind(card_placeholder_oponent))
