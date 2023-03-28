5d2fe724adb78223a84e5359b704e4376ae8f9ad
Fernando Guillen <fguillen.mail@gmail.com>
2023-03-28 22:43:18 +0200

    Monster walk (II)

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Forest Monster and the Cards Championship
+
+## Assets
+
+- Forest walking: https://freesound.org/people/courter/sounds/447822/
+- Forest background: https://freesound.org/people/mirkosukovic/sounds/434017/
+- Forest music: https://freesound.org/people/FoolBoyMedia/sounds/332323/
+- Monster walk: https://freesound.org/people/IlMotografo/sounds/679104/
--- effects/effect_sound.gd
+++ effects/effect_sound.gd
@@ -1,10 +1,19 @@
 extends AudioStreamPlayer
 
+@export var random_pitch := false
 @export var start_at_random_seek := false
+@export var samples: Array[AudioStream]
 
 
 func perform():
 	var seek = 0.0 if not start_at_random_seek else _random_seek()
+	
+	if not samples.is_empty():
+		stream = _random_sample()
+		
+	if random_pitch:
+		pitch_scale = randf_range(0.8, 1.2)
+	
 	play(seek)
 
 
@@ -14,3 +23,7 @@ func finish():
 
 func _random_seek():
 	return randf_range(0.0, stream.get_length())
+
+	
+func _random_sample() -> AudioStream:
+	return samples.pick_random()
